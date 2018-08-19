d9d53cd2d6ce0fe1de1126b713ce4998d6406143
Kevin Egunza <egunza.k@gmail.com>
2018-08-19 13:04:41 +0300

    Add and test an endpoint to post answers to a question

--- app/api_1_0/__init__.py
+++ app/api_1_0/__init__.py
@@ -9,16 +9,50 @@ questions = [
         'question': 'What is the ...'},
     {
         'id': 2,
-        'question': 'How would ...'}
+        'question': 'How would ...'},
+    {
+        'id': 3,
+        'question': 'Is TDD a preferred method in ...'}
 ]
 
-@api.route('/questions/', methods=['GET'])
+answers = [{
+        'id': 1,
+        'answer': 'All editors are ...',
+        'question_id':2
+        },
+        {
+        'id': 2,
+        'answer': 'The most important ...',
+        'question_id':1
+        },
+        {
+        'id': 3,
+        'answer': 'What is th second ...',
+        'question_id':1
+        }]
+
+
+@api.route('/questions/<int:question_id>/answers/', methods=['POST'], strict_slashes=False)
+def create_answer(question_id):
+    if not request.json or not 'answer' in request.json:
+        abort(400)
+    answer = {
+        'id': answers[-1]['id'] + 1,
+        'answer': request.json['answer'],
+        'question_id':question_id
+        }
+    answers.append(answer)
+    response = jsonify({'answer': answer})
+    response.status_code = 201
+    return response
+
+@api.route('/questions/', methods=['GET'], strict_slashes=False)
 def get_questions():
     response = jsonify({'questions': questions})
     response.status_code = 200
     return response
 
-@api.route('/questions/<int:question_id>', methods=['GET'])
+@api.route('/questions/<int:question_id>/', methods=['GET'], strict_slashes=False)
 def get_question(question_id):
     question = [question for question in questions if question['id'] == question_id]
     if len(question) == 0:
@@ -27,7 +61,7 @@ def get_question(question_id):
     response.status_code = 200
     return response    
 
-@api.route('/questions/', methods=['POST'])
+@api.route('/questions/', methods=['POST'], strict_slashes=False)
 def create_question():
     if not request.json or not 'question' in request.json:
         abort(400)
@@ -36,14 +70,23 @@ def create_question():
         'questiion': request.json['question'],
         }
     questions.append(question)
-    response = jsonify({'questiion': question})
+    response = jsonify({'question': question})
     response.status_code = 201
     return response
 
-@api.route('/questions/<int:question_id>', methods=['DELETE'])
+@api.route('/questions/<int:question_id>/', methods=['DELETE'], strict_slashes=False)
 def delete_question(question_id):
     question = [question for question in questions if question['id'] == question_id]
     if len(question) == 0:
         abort(404)
     questions.remove(question[0])
     return jsonify({'result': True})
+
+
+
+@api.route('/questions/<int:question_id>/answers/', methods=['GET'], strict_slashes=False)
+def get_answers(question_id):
+    ans = [answer for answer in answers if answer['question_id'] == question_id]
+    response = jsonify({'answers': ans}) 
+    response.status_code = 200
+    return response
--- tests.py
+++ tests.py
@@ -3,13 +3,14 @@ import os
 import json
 from app import create_app
 
-class QuestionsTestCase(unittest.TestCase):
+class EndpointsTestCase(unittest.TestCase):
 
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client
-        self.question = {'question': 'Define the various programming paridigms'}
+        self.question = {"question": "Define the various programming paridigms"}
+        self.answer = {"answer": "Object oriented, Procedural and Structured paradigms."}
                                 
 
     def test_api_can_get_all_questions(self):
@@ -25,11 +26,11 @@ class QuestionsTestCase(unittest.TestCase):
         self.assertIn('How would ...', str(res.data))
 
 
-    """ def test_question_creation(self):
+    def test_question_creation(self):
         
-        res = self.client().post('/api/v1.0/questions/', data=self.question)
+        res = self.client().post('/api/v1.0/questions/', json=self.question)
         self.assertEqual(res.status_code, 201)
-        self.assertIn('Define the various programming paridigms', str(res.data)) """
+        self.assertIn('Define the various programming paridigms', str(res.data))
 
     def test_question_deletion(self):
         
@@ -38,6 +39,20 @@ class QuestionsTestCase(unittest.TestCase):
         # Test to see if it exists, should return a 404
         result = self.client().get('/api/v1.0/questions/1')
         self.assertEqual(result.status_code, 404)
+
+    """ def test_api_can_get_all_answers_to_a_question(self):
+        
+        res = self.client().get('/api/v1.0/questions/2/answers/')
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('All editors are ...', str(res.data)) """
+    
+    def test_answer_creation(self):
+            
+        res = self.client().post('/api/v1.0/questions/2/answers/', json=self.answer)
+        self.assertEqual(res.status_code, 201)
+        self.assertIn('Object oriented, Procedural and Structured paradigms.', str(res.data))
+
+
 
 if __name__ == "__main__":
     unittest.main()
